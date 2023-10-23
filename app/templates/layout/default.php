0744f1a84a27f358f40a11c8d1aa05c910e39471
Стив <kenethra.74@gmail.com>
2023-10-22 21:55:43 -0600

    feat: sidebar

--- app/templates/layout/default.php
+++ app/templates/layout/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -18,6 +19,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,9 +34,60 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
-</head>
-<body>
+    <style>
+        .sidebar {
+            width: 250px;
+            background-color: #333;
+            color: #fff;
+            position: fixed;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
 
+        .sidebar ul {
+            list-style: none;
+            padding: 0;
+            margin-top: 15px;
+        }
+
+        .sidebar li {
+            padding: 10px;
+        }
+
+        .sidebar li>a {
+            cursor: pointer;
+        }
+
+        .main {
+            margin-left: 260px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="sidebar">
+        <ul>
+            <li>
+                <a href="carrera">Carreras</a>
+            </li>
+            <li>
+                <a href="curso">Cursos</a>
+            </li>
+            <li>
+                <a href="semestre">Semestres</a>
+            </li>
+            <li>
+                <a href="seccion">Secciones</a>
+            </li>
+            <li>
+                <a href="alumno">Alumnos</a>
+            </li>
+        </ul>
+    </div>
     <nav class="top-nav">
         <div class="top-nav-title">
         </div>
@@ -50,4 +103,5 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <footer>
     </footer>
 </body>
+
 </html>
